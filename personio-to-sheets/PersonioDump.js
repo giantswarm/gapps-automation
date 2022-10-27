b3e96f5f013ec79bc1bc51a27c6cdca3a98c98be
Jonas Zeiger <jonas.zeiger@talpidae.net>
2022-10-27 12:46:19 +0200

    Complete format support #22828 (#1)
    * Complete format support
    
    * Changes from review, fixes
    
    * Fix spelling mistake
    
    Co-authored-by: Marcel <marcel.mueller1@rwth-aachen.de>
    
    * Add explicit skipped-task logging
    
    Co-authored-by: Jonas Zeiger <jonas@giantswarm.io>
    Co-authored-by: Marcel <marcel.mueller1@rwth-aachen.de>

--- personio-to-sheets/PersonioDump.js
+++ personio-to-sheets/PersonioDump.js
@@ -1,21 +1,21 @@
 /**
  * Dump Personio Context into a Sheet
  *
- *   - The source URL, token and target sheets for sync process
- *     must be specified as ScriptProperties (also see setProperties()).
+ *   - The source URL, tokens and target sheets for sync process
+ *     must be specified as ScriptProperties (see setProperties() helper function).
  *
  *     FORMAT:
- *          Script Property Key: SHEET_ID
+ *          Script Property Key: PersonioDump.SHEET_ID
  *          Script Property Value: FULL_PERSONIO_API_URL|PERSONIO_CLIENT_ID|PERSONIO_CLIENT_SECRET
  *
  *     EXAMPLE:
- *         "PersonioDump.1wX-VnjLVkBL74SC-8qMt_oeib4VGMlDpZuJzrd_NZUE": "/company/employees.json|lkjklasdj|lkjakasd|"
+ *         "PersonioDump.1wX-VnjLVkBL74SC-8qMt_oeib4VGMlDpZuJzrd_NZUE": "/company/employees.json|lkjklasdj|lkjakasd"
  *
- *   - The target sheet must be accessible (shared with) the account running the script, for example:
+ *   - The target sheet must be accessible by the account running the script, for example:
  *
- *     personio-sync@gapps-automation-2022.iam.gserviceaccount.com
+ *     automation@giantswarm.io
  *
- * This script uses the Advanced Sheets Service (make sure to enable it in the Google Cloud Project).
+ * This script uses the Advanced Sheets Service, make sure to enable it in the Google Cloud Project.
  */
 
 
@@ -53,15 +53,16 @@ function dumpPersonio() {
         } catch (e) {
             Logger.log('Failed to fetch Personio data for sheet %s: %s', task.spreadsheetId, e.message);
             firstError = firstError || e;
+            continue;
         }
 
-        // TODO Handle some special data/formats (for example lists, images or other blobs)?
         let relations = null;
         try {
             relations = transformPersonioDataToRelations_(data);
         } catch (e) {
-            Logger.log('Failed to write rows to sheet %s: %s', task.spreadsheetId, e.message);
+            Logger.log('Failed to transform Personio data for sheet %s: %s', task.spreadsheetId, e.message);
             firstError = firstError || e;
+            continue;
         }
 
         try {
@@ -78,6 +79,17 @@ function dumpPersonio() {
 }
 
 
+/** Sanitize delay minutes input.
+ *
+ * ClockTriggerBuilder supported only a limited number of values, see:
+ * https://developers.google.com/apps-script/reference/script/clock-trigger-builder#everyMinutes(Integer)
+ */
+function sanitizeDelayMinutes_(delayMinutes) {
+    return [1, 5, 10, 15, 30].reduceRight((v, prev) =>
+        typeof +delayMinutes === 'number' && v <= +delayMinutes ? v : prev);
+}
+
+
 /** Uninstall time based execution trigger for this script. */
 function uninstall() {
     // Remove pre-existing triggers
@@ -91,17 +103,18 @@ function uninstall() {
 }
 
 
-/** Setup for periodic execution and do some checks. */
+/** Setup for periodic execution and do some checks.
+ * Supported values for delayMinutes: 1, 5, 10, 15 or 30
+ */
 function install(delayMinutes) {
     uninstall();
 
-    Logger.log("Installing time based trigger: %s", delayMinutes);
-    const delay = delayMinutes || 30;
+    const delay = sanitizeDelayMinutes_(delayMinutes);
+    Logger.log("Installing time based trigger (every %s minutes)", delayMinutes);
 
     ScriptApp.newTrigger(TRIGGER_HANDLER_FUNCTION)
         .timeBased()
         .everyMinutes(delay)
-        //.everyHours(1)
         .create();
 
     Logger.log("Installed time based trigger for %s every %s minutes", TRIGGER_HANDLER_FUNCTION, delay);
@@ -134,24 +147,32 @@ function getTasks_() {
         if (!safeKey.startsWith(PROPERTY_PREFIX))
             continue;
 
-        // TODO Extract sheet ID from sheets URL if such is present
         const spreadsheetId = safeKey.replace(PROPERTY_PREFIX, '');
         if (!spreadsheetId) {
             continue;
         }
 
         try {
-            // TODO Validate object and output proper log messsage
-            const apiParts = properties[key].trim().split('|');
-            const sourceSpec = {
-                url: apiParts[0].trim(),
-                clientId: apiParts[1].trim(),
-                clientSecret: apiParts[2].trim()
-            };
+            const rawProperty = properties[key] || '';
+            const apiParts = rawProperty.trim().split('|');
 
-            tasks.push({spreadsheetId: spreadsheetId, source: sourceSpec});
+            if (apiParts.length === 3) {
+                const sourceSpec = {
+                    url: apiParts[0].trim(),
+                    clientId: apiParts[1].trim(),
+                    clientSecret: apiParts[2].trim()
+                };
+
+                if (sourceSpec.url && sourceSpec.clientId && sourceSpec.clientSecret) {
+                    tasks.push({spreadsheetId: spreadsheetId, source: sourceSpec});
+                } else {
+                    Logger.log("Skipped task: Empty fields in property value for key %s: %s", key, rawProperty);
+                }
+            } else {
+                Logger.log("Skipped task: Expected 3 fields (URL, CLIENT_ID, CLIENT_SECRET) in property value for key %s: %s", key, rawProperty);
+            }
         } catch (e) {
-            Logger.log('Incorrect API config for property key %s: %s', key, e.message);
+            Logger.log('Skipped task: Incorrect API config for property key %s: %s', key, e.message);
         }
     }
 
@@ -159,123 +180,265 @@ function getTasks_() {
 }
 
 
-/** Transform objects contained in Persionio API response data.
+/** Transform objects from Persionio API v1 response data to normalized relations.
  *
  *  Output Example:
  *
  *  {
- *      Employee: { headers: { first_name: "First Name"}, rows: [["First Name"], ["Jonas"]]},
- *      TimeOff: { ... }
+ *      Employee: { headers: { first_name: {title: "First Name"}}, rows: [["First Name"], ["Jonas"]]},
+ *      Department: { ... }
  *  }
  *
  */
 function transformPersonioDataToRelations_(data) {
 
-    const relations = {};  // ie. types
+    const relations = {};  // ie. tables in a relational schema
 
-    for (const item of data) {
+    const hasParentOfType = (parents, relType) => !!parents.find(parent => parent.type === relType);
 
-        // make multiple relations (by field type) support optional?
-        if (!item || !item.type)
-            continue;
+    const unboxValue = boxedValue => boxedValue?.value !== undefined ? boxedValue.value : boxedValue;
 
-        // TODO Implement more fields here?
+    const getItemId = (item) => {
+        if (item?.attributes) {
+            return unboxValue(item.attributes['id']);
+        }
+        // null/undefined: this item has no ID (ie. item == null)
+        return null;
+    };
+
+    // scan an object, recursively
+    // returns the item type (relation) if the object was handled, null otherwise
+    const scanObject = (item, parents) => {
+
         // map attribute labels to columns
-        const attrs = item.attributes;
-        if (!attrs)
-            continue;
+        const attributes = item?.attributes;
+        const itemType = item?.type;
+        if (!Util.isObject(attributes) || !itemType)
+            throw new Error(`Unknown object without type or attributes: ${JSON.stringify(item)}`);
 
-        if (relations[item.type] === undefined) {
-            relations[item.type] = {headers: {}, rows: []};
+        if (hasParentOfType(parents, itemType))
+            return itemType;
+
+        if (!relations[itemType]) {
+            relations[itemType] = {headers: {}, rows: [], ids: {}};
         }
 
-        const relation = relations[item.type];
+        const relation = relations[itemType];
 
-        for (const id in attrs) {
-            if (!relation.headers[id]) {
-                // track columns
-                // TODO Can we guarantee they are always the same for each object?
-                const attr = attrs[id];
-                const label = (attr.label || '').trim();
-                const uniform_id = (attr.uniform_id || '').trim();
-                relation.headers[id] = label || uniform_id || id.trim();
+        for (const id in attributes) {
+
+            if (relation.headers[id])
+                continue; // already registered
+
+            const attribute = attributes[id];
+            const value = unboxValue(attribute);
+
+            const values = Array.isArray(value) ? value : [value];
+            for (const unpackedValue of values) {
+                if (unpackedValue === null || unpackedValue === undefined) {
+                    // skip or register later
+                    // NOTE: This will drop columns/relations with all values set to NULL
+                    continue;
+                }
+
+                // We support varying values in arrays, but that shouldn't occur
+                let foreignType = null;
+                if (Util.isObject(unpackedValue)) {
+                    foreignType = scanObject(unpackedValue, parents.concat([item]));
+                    if (!foreignType) {
+                        continue;
+                    }
+                    // nested, possibly circular type was registered
+                }
+
+                // add column header
+                const label = (attribute?.label || '').trim();
+                const uniform_id = (attribute?.uniform_id || '').trim();
+                relation.headers[id] = {
+                    title: (label || uniform_id || id.trim()) + (foreignType ? '_' + foreignType + '_id' : ''),
+                    foreignType: foreignType
+                };
             }
         }
-    }
 
-    // first rows for each relation (headers)
-    for (const relType in relations) {
-        const relation = relations[relType];
-        relation.rows.push(Object.values(relation.headers));
-    }
+        return itemType;
+    };
 
-    // TODO Are there more field values (except attributes)?
-    for (const item of data) {
+    // convert values recursively
+    const convertObject = (item, parents) => {
 
-        if (!item || !item.type)
-            continue;
-
-        const relation = relations[item.type];
+        const itemType = item?.type;
+        const relation = relations[itemType];
         if (!relation)
-            continue;
+            return;
+
+        if (hasParentOfType(parents, itemType))
+            return;
+
+        // filter out duplicate rows by ID
+        const itemId = getItemId(item);
+        if (itemId != null) {
+            if (relation.ids[itemId]) {
+                return;
+            } else {
+                relation.ids[itemId] = true;
+            }
+        }
 
         const row = [];
-        for (const header in relation.headers) {
-            row.push(item.attributes[header]?.value);
+        for (const column in relation.headers) {
+            const header = relation.headers[column];
+            const value = unboxValue(item.attributes[column]);
+
+            const values = Array.isArray(value) ? value : [value];
+            let fields = [];
+            for (const unpackedValue of values) {
+                if (header.foreignType) {
+                    // nested type, just store id and row in a separate relation
+                    const itemId = getItemId(unpackedValue);
+                    fields.push(itemId);
+                    convertObject(unpackedValue, parents.concat([item]));
+                } else {
+                    fields.push(unpackedValue);
+                }
+            }
+
+            // plain value null/undefined -> ''
+            // arrays joined by ',' (comma)
+            row.push(fields.map(v => v == null ? '' : v).join(','));
         }
+
         relation.rows.push(row);
+    };
+
+    // #1 Build schema (scan data returned from API)
+    for (const item of data) {
+        scanObject(item, []);
+    }
+
+    // #2 Set headers (first rows for each relation)
+    for (const relType in relations) {
+        const relation = relations[relType];
+        relation.rows.push(Object.values(relation.headers).map(header => header.title));
+    }
+
+    // #3 Convert data (rows)
+    for (const item of data) {
+        convertObject(item, []);
     }
 
     return relations;
 }
 
 
-/** Creates a "custom" sheet with the specified name (if it doesn't exist) and returns the sheetId. */
-function addOrGetSheet_(spreadsheetId, sheetTitle) {
-    try {
-        const requests = [{
-            'addSheet': {
-                'properties': {
-                    'title': sheetTitle
-                }
-            }
-        }];
+/** Creates a "custom" sheet with the specified name (if it doesn't exist) and returns the sheet properties. */
+function getOrAddSheet_(spreadsheetId, sheetTitle) {
+    const spreadsheet = Sheets.Spreadsheets.get(spreadsheetId);
+    if (!spreadsheet)
+        throw new Error(`Specified spreadsheet ${spreadsheetId} does not exist or is not accessible`);
 
-        const response = Sheets.Spreadsheets.batchUpdate({'requests': requests}, spreadsheetId);
-        return response.replies[0].addSheet.properties.sheetId;
-    } catch (e) {
-        if (e.details.code === 400) {
-            try {
-                const existingSheet = Sheets.Spreadsheets.get(spreadsheetId).sheets.find(sheet => sheet.properties.title === sheetTitle);
-                if (existingSheet) {
-                    return existingSheet.sheetId;
-                }
-            } catch (e1) {
-                Logger.log('Failed to lookup existing sheet: %s', e.message);
-            }
-        }
-        Logger.log('Failed to create new sheet: %s', e.message);
-        throw e;
+    const existingSheet = spreadsheet.sheets.find(sheet => sheet.properties.title === sheetTitle);
+    if (existingSheet) {
+        return existingSheet.properties;
     }
+
+    const batch = {
+        requests: [{
+            addSheet: {
+                properties: {
+                    title: sheetTitle
+                }
+            }
+        }]
+    };
+    const response = Sheets.Spreadsheets.batchUpdate(batch, spreadsheetId);
+    return response.replies[0].addSheet.properties;
 }
 
 
+/** Write relations structure to the specified spreadsheet, creating the necessary sheets. */
 function writeRelationsToSheet_(spreadsheetId, relations, valueInputOption) {
 
-    // Store each relation in a corresponding sheet
-    const request = {
-        'valueInputOption': valueInputOption,
-        'data': Object.entries(relations).map(([relType, relation]) => {
-            // add sheet or use existing
-            addOrGetSheet_(spreadsheetId, relType);
-            return {
-                range: "'" + relType + "'",
-                majorDimension: 'ROWS',
-                values: relation.rows
-            }
-        })
+    // Extended Value for CellData fields see:
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/other#extendedvalue
+    const toExtendedValue = (value) => {
+        switch (typeof value) {
+            case 'boolean':
+                return {boolValue: value};
+            case 'number': // fall-through
+            case 'bigint':
+                return {numberValue: value};
+            case 'string':
+                return {stringValue: value};
+            default:  // null, undefined, object, symbol, function
+                return {errorValue: {type: 'NULL_VALUE'}};
+        }
     };
 
-    // TODO Do something with the reponse? Log number of changed cells?
-    Sheets.Spreadsheets.Values.batchUpdate(request, spreadsheetId);
+
+    // Prepare Sheets API requests in advance
+    const batch = {requests: []};
+
+    for (const [relType, relation] of Object.entries(relations)) {
+
+        const sheetProperties = getOrAddSheet_(spreadsheetId, relType);
+        const sheetId = getOrAddSheet_(spreadsheetId, relType).sheetId;
+        const rowCount = sheetProperties.gridProperties.rowCount;
+        const columnCount = sheetProperties.gridProperties.columnCount;
+        const targetRowCount = relation.rows.length;
+        const targetColumnCount = relation.rows.length ? relation.rows[0].length : 0
+
+        // #1 Ensure correct dimensions (enough rows/columns to insert data)
+        if (targetRowCount > rowCount) {
+            batch.requests.push({
+                appendDimension: {
+                    sheetId: sheetId,
+                    dimension: 'ROWS',
+                    length: targetRowCount - rowCount
+                }
+            });
+        }
+        if (targetColumnCount > columnCount) {
+            batch.requests.push({
+                appendDimension: {
+                    sheetId: sheetId,
+                    dimension: 'COLUMNS',
+                    length: targetColumnCount - columnCount
+                }
+            });
+        }
+
+        // #2 Clear the whole worksheet, preserving formats
+        batch.requests.push(
+            {
+                updateCells: {
+                    range: {
+                        sheetId: sheetId
+                    },
+                    fields: 'userEnteredValue'
+                }
+            });
+
+        // #3 Insert the data rows
+        batch.requests.push(
+            {
+                updateCells: {
+                    range: {
+                        sheetId: sheetId,
+                        startRowIndex: 0,
+                        endRowIndex: relation.rows.length,
+                        startColumnIndex: 0,
+                        endColumnIndex: relation.rows.length ? relation.rows[0].length : 0
+                    },
+                    fields: 'userEnteredValue',
+                    rows: relation.rows.map(row => ({
+                        values: row.map(v => ({
+                            userEnteredValue: toExtendedValue(v)
+                        }))
+                    }))
+                }
+            });
+    }
+
+    Sheets.Spreadsheets.batchUpdate(batch, spreadsheetId);
 }
