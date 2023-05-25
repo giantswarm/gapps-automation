6cc929c929cda6027d187a8d1beb18f89eeead64
Jonas Zeiger <jonas@giantswarm.io>
2023-05-25 11:35:14 +0200

    feat: fall back to out type for eventType outOfOffice

--- sync-timeoffs/SyncTimeOffs.js
+++ sync-timeoffs/SyncTimeOffs.js
@@ -268,8 +268,6 @@ async function unsyncTimeOffs_(title) {
             const allEvents = await queryCalendarEvents_(calendar, 'primary', fetchTimeMin, fetchTimeMax);
             Util.shuffleArray(allEvents);
 
-            let failCount = 0;
-            const processedTimeOffIds = {};
             for (const event of allEvents) {
                 const timeOffId = +event.extendedProperties?.private?.timeOffId;
                 if (timeOffId && (event.summary || '').includes(title)) {
@@ -884,16 +882,20 @@ function convertOutOfOfficeToTimeOff_(timeOffTypeConfig, employee, event, existi
 
     let timeOffType = timeOffTypeConfig.findByKeywordMatch(event.summary || '');
     if (!timeOffType) {
-        if (!existingTimeOff) {
-            return undefined;
+        if (existingTimeOff) {
+            const previousType = timeOffTypeConfig.findById(existingTimeOff.typeId);
+            if (previousType) {
+                timeOffType = previousType;
+            }
         }
 
-        const previousType = timeOffTypeConfig.findById(existingTimeOff.typeId);
-        if (!previousType) {
-            return undefined;
+        if (!timeOffType && event.eventType === 'outOfOffice') {
+            timeOffType = timeOffTypeConfig.findByKeywordMatch('out');
         }
 
-        timeOffType = previousType;
+        if (!timeOffType) {
+            return undefined;
+        }
     }
 
     const halfDaysAllowed = !!timeOffType.attributes?.half_day_requests_enabled;
